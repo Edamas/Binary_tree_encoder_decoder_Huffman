4b80de5c33a9f213728a329db6fd975135734df1
Elysio Neto <49333427+Edamas@users.noreply.github.com>
2022-02-22 01:20:19 -0300

    +Corrections +speed

--- String2BinaryTree.py
+++ String2BinaryTree.py
@@ -1,96 +1,99 @@
-# data_input is the character, symbol, numbers sequence (as input text)
-
-data_input = 100 * ''.join(key * int(100 * value) for key, value in {' ': 15, 'a': 14.63, 'e': 12.57, 'o': 10.73, 's': 7.81, 'r': 6.53, 'i': 6.18, 'n': 5.05, 'd': 4.99, 'm': 4.74, 'u': 4.63, 't': 4.34, 'c': 3.88, 'l': 2.78, 'p': 2.52, 'v': 1.67, 'g': 1.3, 'h': 1.28, 'q': 1.2, 'b': 1.04, 'f': 1.02, 'z': 0.47, 'j': 0.4, 'x': 0.21, 'k': 0.02, 'w': 0.01, 'y': 0.01}.items()) 
-
-# this is the base of our data_input, but you can change it above and print it bellow
-print('Portuguese alphabet frequency in %:\n', *{' ': 15, 'a': 14.63, 'e': 12.57, 'o': 10.73, 's': 7.81, 'r': 6.53, 'i': 6.18, 'n': 5.05, 'd': 4.99, 'm': 4.74, 'u': 4.63, 't': 4.34, 'c': 3.88, 'l': 2.78, 'p': 2.52, 'v': 1.67, 'g': 1.3, 'h': 1.28, 'q': 1.2, 'b': 1.04, 'f': 1.02, 'z': 0.47, 'j': 0.4, 'x': 0.21, 'k': 0.02, 'w': 0.01, 'y': 0.01}.items())
-
-print('_' * 100)
+file_name = r"EMS.1928-1955 - palavras.txt"
+with open(file_name, 'rt', encoding='utf-8') as file:
+    data_input = file.read()
 
 # get the frequency (in %) for unique characters
 lenght = len(data_input)
-frequencies, characters = [], []
-for character in data_input:
-    if character not in characters:
-        characters.append(character)
-        count = data_input.count(character)
-        frequencies.append(100 * count/lenght)
+freq_chars = sorted([[100 * data_input.count(character)/lenght, character] for character in list(set(data_input))])
 
-# always using the ascending frequency of characters order
-freq_chars = [[frequencies[index], characters[index]] for index in range(len(characters))]
-freq_chars = sorted(freq_chars)
+print('Frequency for each character:')
+print(*[f'{str(round(freq, 5)).ljust(7, "0")}%\t "{char}"' for freq, char in freq_chars], sep='\n')
 
 # compute the groups of characters (leaves) and groups of the nodes combining them (branch) until we get one group with all characters (the trunk)
 allgroups = []
 while freq_chars:
     if len(freq_chars) >= 2:
-        old0, old1 = freq_chars.pop(0), freq_chars.pop(0)
+        old0, old1 = [freq_chars.pop(0) for _ in range(2)]
         allgroups.extend([old0, old1])
         freq_chars = sorted(freq_chars + [[old0[0] + old1[0], old0[1] + old1[1]]])
     else:
         old = freq_chars.pop(0)
         allgroups.append(old)
 
-# allchars is the combination of all chars in the computed sequence
-allchars = [group for group in allgroups if len(group[1]) == max([len(x) for _, x in allgroups])][0][1]
+allgroups = sorted(allgroups, key=lambda x: [-len(x[1]), -x[0]])
 
-# process the bits for each pair (left, right = above node)
-left_right = {}
-for freq1, char1 in sorted(allgroups, key=lambda x: [-len(x[1]), -x[0]]):
-        for freq2, char2 in sorted(allgroups, key=lambda x: [-len(x[1]), -x[0]]):
-            for freq3, char3 in sorted(allgroups, key=lambda x: [-len(x[1]), -x[0]]):
+# process the bits for each pair (left + right = above node)
+left_right = {char: '' for _, char in allgroups}
+for freq1, char1 in allgroups:
+        for freq2, char2 in allgroups:
+            for freq3, char3 in allgroups:
                 if char2 + char3 == char1:
                     if char1.startswith(char2):
                         if char1 in left_right:
-                            left_right[char2] = left_right[char1] + '0'
-                            left_right[char3] = left_right[char1] + '1'
-                        else:
-                            left_right[char2] = '0'
-                            left_right[char3] = '1'
+                            above_bit = left_right[char1]
+                            left_right[char2], left_right[char3] = [above_bit, above_bit]
+                        left_right[char2] += '0'
+                        left_right[char3] += '1'
+
+                            
+# sorting bits by lenght, then ascending
+left_right = sorted(left_right.items(), key=lambda x: [len(x[1]), x[1]])
 
 # filter the single characters in left_right
-conversion_table = {char: binary for char, binary in sorted(left_right.items(), key=lambda x: [len(x[1]), x[1]]) if len(char) == 1}
-print('Conversion_table: (From the most to the least frequent character)\n', *conversion_table.items(), sep ='\t')
+conversion_table = {char: binary for char, binary in left_right if len(char) == 1}
+print('\nConversion_table: (From the most to the least frequent character')
 
-# compute the binary and converted (encripted) version of data_input
-def encrypt(string, conversion_table):
-    binary, crypto = '', ''
-    for char in string:
-        bin_prefix = '0' * (8-len(bin(ord(char))[2:]))
-        binary += bin_prefix + bin(ord(char))[2:]
-        crypto += conversion_table[char]
-    return binary, crypto
 
-# try to decrypt the encripted bits
+printables = [f'{binary.ljust(30)}\t{char.ljust(10)}' for char, binary in conversion_table.items() if char.isprintable()]
+print('\nPrintables')
+print(*printables, sep='\n')
+
+non_printables = [f'{binary.ljust(30)}\t{char.ljust(10)}' for char, binary in conversion_table.items() if not char.isprintable()]
+if non_printables:
+    print('\nNon printables:\n')
+    print(*non_printables, sep='\n')
+
+
+# compute the converted (encripted) and the binary version for data_input
+def encrypt_bin(string, conversion_table):
+    return ''.join([bin(ord(char))[2:].rjust(8, '0') for char in string])
+
+
+def encrypt_cry(string, conversion_table):
+    return ''.join([conversion_table[char] for char in string])
+
+
+# decoding bits
 def decrypt(bits, conversion_table):
-    temp, decrypted = '', ''
+    temp, decrypted = '', []
+    decode_table = {binary_code: character for character, binary_code in conversion_table.items()}
     for bit in bits:
-        temp += bit
-        if temp in conversion_table.values():
-            decrypted += [key for key, value in conversion_table.items() if value == temp][0]
+        temp = temp + bit
+        if temp in decode_table:
+            decrypted.append(decode_table[temp])
             temp = ''
-    return decrypted
+    return ''.join(decrypted)
 
-print('_' * 100, '\nSAMPLES:')
-sample = 'python for everyone'
-sample_bin, sample_enc = encrypt(sample, conversion_table)
-print(f'''The string "{sample}" converted using these two methods:
-    Binary method: ({len(sample_bin)} bits)
-        {sample_bin}
-    Encryption method: ( {len(sample_enc)} bits)
-        {sample_enc}''')
-
+print('-' * 100)
 # process and finish analysis using data_input
-binary, crypto = encrypt(data_input, conversion_table)
+print('Encoding to standard 8 bits...')
+binary = encrypt_bin(data_input, conversion_table)
+
+print('Encoding binary tree (encryption)...')
+crypto = encrypt_cry(data_input, conversion_table)
+
+print('Decoding encrypted bits...')
 decrypted = decrypt(binary, conversion_table)
 len_cry, len_bin = len(crypto), len(binary)
 
 print('_' * 100, '\nANALYSIS FOR data_input:')
+print(f'\ndata_input:\n{data_input[:100]}\n')
+
 print(f'data_input type:   {type(data_input)}')
-print(f'data_input size:   {lenght:,} characters (bytes)')
-print(f'Binary size:       {len_bin:,} bits ({round(100 * len_bin / len_bin, 1)}%)\t8.00 bits (average) per character.')
+print(f'data_input size:   {lenght:,}b (bytes or characters)')
+print(f'Binary size:       {len_bin:,} bits ({round(100 * len_bin / len_bin, 1)}%)\t8.00 bits per character.')
 print(f'Cryptography size: {len_cry:,} bits ( {round(100 * len_cry / len_bin, 1)}%)\t{round(len_cry/len(data_input), 2)} bits (average) per character.')
 
 # the real benefit for storage or processing:
 print('_' * 100, '\nCONCLUSION:')
-print('You saved', round((len_bin - len_cry)/1024/1024, 2), 'MB from', round(len_bin/1024/1024, 2), 'MB using this encryption method!')
+print(f'You saved {round((len_bin - len_cry)/8/1024/1024, 5)}MB from {round(len_bin/8/1024/1024, 5)}MB using this encryption method!')
